0d580ff96b950434f74273d9933fcb50983d9e8c
Enis Berk Çoban <EnisBerk@users.noreply.github.com>
2021-12-09 12:36:12 -0500

    init day 9

---
+++ day9/test_tools.py
@@ -0,0 +1,12 @@
+from tools import *
+
+
+input_text = '''2199943210
+3987894921
+9856789892
+8767896789
+9899965678'''
+
+def test_part1():
+    assert part1(input_text) == 15
+    # assert part2(input_text) == 61229
---
+++ day9/tools.py
@@ -0,0 +1,26 @@
+'''
+tools for advent code day 7
+'''
+from collections import Counter
+
+def load_data(text):
+    '''
+    load data from file
+    '''
+    data = text.strip().split('\n')
+    # data = [map(int, x) for x in data]
+    return data
+
+
+def part1(text):
+    '''
+    find count of unique integers 1,4,7,8
+        int:len
+        1:2,4:4,7:3,8:7
+    '''
+    data = load_data(text)
+    pass
+
+def part2(text):
+    data = load_data(text)
+    pass
