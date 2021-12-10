f01348d7a72d694e47af0e37c420cc97ce7e1b03
Enis Berk Çoban <EnisBerk@users.noreply.github.com>
2021-12-10 10:06:10 -0500

    init day 10

---
+++ day10/tools.py
@@ -0,0 +1,27 @@
+'''
+tools for advent code day 10
+'''
+from collections import Counter
+from operator import le
+
+
+def load_data(text):
+    '''
+    load data from file
+    '''
+    data = text.strip().split('\n')
+    data = [[int(i) for i in x] for x in data]
+ 
+    return data
+
+def part1(data):
+    '''
+    part 1
+    '''
+    return None
+
+def part2(data):
+    '''
+    part 2
+    '''
+    return None
